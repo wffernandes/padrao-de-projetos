cc1f20ccc5f42e549f3130746a75cefdb2175799
wffernandes <wffernandes01@gmail.com>
2026-08-31 19:22:57 -0300

    feat: add abstract factory.

---
+++ creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/factory/abstractFactory/USRulesAbstractFactory.java
@@ -0,0 +1,19 @@
+package br.com.cod3r.abstractfactory.apple.factory.abstractFactory;
+
+import br.com.cod3r.abstractfactory.apple.model.certificate.Certificate;
+import br.com.cod3r.abstractfactory.apple.model.certificate.USCertificate;
+import br.com.cod3r.abstractfactory.apple.model.packing.Packing;
+import br.com.cod3r.abstractfactory.apple.model.packing.USPacking;
+
+public class USRulesAbstractFactory implements CountryRulesAbstractFactory {
+
+    @Override
+    public Certificate getCertificates() {
+        return new USCertificate();
+    }
+
+    @Override
+    public Packing getPacking() {
+        return new USPacking();
+    }
+}
