c18874e43bc17152b8ab85894bd75d104a42e724
wffernandes <wffernandes01@gmail.com>
2026-09-06 15:30:03 -0300

    feat: add project memento.

---
+++ behavioral-patterns/src/main/java/br/com/cod3r/memento/swing/component/TextAreaWithMemory.java
@@ -0,0 +1,33 @@
+package br.com.cod3r.memento.swing.component;
+
+import br.com.cod3r.memento.swing.memory.Memento;
+
+import javax.swing.JTextArea;
+
+public class TextAreaWithMemory extends JTextArea {
+	private static final long serialVersionUID = 2393587108160522742L;
+	
+	public TextAreaWithMemory(int rows, int columns) {
+		super(rows, columns);
+	}
+
+	public TextAreaMemento save() {
+		return new TextAreaMemento(getText());
+	}
+
+	public void restore(TextAreaMemento memento) {
+		setText(memento.getState());
+	}
+
+	public static class TextAreaMemento implements Memento {
+		private String text;
+
+		public TextAreaMemento(String text) {
+			this.text = text;
+		}
+
+		private String getState() {
+			return text;
+		}
+	}
+}
